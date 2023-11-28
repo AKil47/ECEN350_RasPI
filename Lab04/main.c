d0e92a4cb783c8942d554a61a6202af2d8373890
Akilan Manivannan <amanivannan@outlook.com>
2023-11-28 17:29:12 -0600

    Lab 4

--- Lab04/main.c
+++ Lab04/main.c
@@ -3,10 +3,15 @@
 #include <stdio.h>
 
 extern long long int my_mul(long long int a, long long int b);
+extern long long int factorial(long long int n);
 
 int main(void)
 {
-    long long int a = my_mul(3, 5);
-    printf("Result of my_mul(3, 5) = %lld\n", a);
+	long long int a = my_mul(3, 5);
+	printf("Result of my_mul(3, 5) = %lld\n", a);
+
+	long long int result = factorial(5);
+	printf("Result of factorial(3) = %lld\n", result);
+
     return 0;
 }
