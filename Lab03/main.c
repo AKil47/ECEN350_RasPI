f44249576f11e388d5da5fff0ef3380fd6f97886
Akilan Manivannan <amanivannan@outlook.com>
2023-11-28 17:28:46 -0600

    Lab 3

--- Lab03/main.c
+++ Lab03/main.c
@@ -2,11 +2,22 @@
 
 #include <stdio.h>
 
-extern long long int test(long long int a, long long int b);
+//extern long long int test(long long int a, long long int b);
+//extern long long int* lab03b();
+extern long long int* lab03c();
+
 
 int main(void)
 {
-    long long int a = test(3, 5);
-    printf("Result of test(3, 5) = %lld\n", a);
-    return 0;
+    //long long int a = test(3, 5);
+    //printf("Result of test(3, 5) = %lld\n", a);
+
+	//printf("%d\n", *lab03b());
+	long long int* my_array = lab03c();
+	for (int i = 0; i < 10; i++) {
+		printf("%d, ", my_array[i]);
+	}
+	printf("\n");
+
+	return 0;
 }
