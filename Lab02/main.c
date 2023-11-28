f9e5b6b2376d85dabd97fb1170d2de80af655a59
Akilan Manivannan <amanivannan@outlook.com>
2023-11-28 17:28:25 -0600

    Lab 2

--- Lab02/main.c
+++ Lab02/main.c
@@ -1,7 +1,12 @@
-extern long long int test();
+//extern long long int test();
+extern void lab02c(long long int a);
+//extern long long int* lab02d (long long int a);
+
 
 int main(void)
 {
-	test();
+	//test();
+	lab02c(21);
+	//printf("%d", *lab02d(2138));
     return 0;
 }
